a221f68feb4255954979bcae71f8c294de0bb8ac
Kevin Guerrero <kevinjoseguerrero44@gmail.com>
2024-10-14 16:00:24 -0500

    se removio el metodo static

--- src/main/java/com/Aplication/Services/TurnoService.java
+++ src/main/java/com/Aplication/Services/TurnoService.java
@@ -13,10 +13,10 @@ import java.util.Optional;
 public class TurnoService {
 
     @Autowired
-    private static TurnoRepository turnoRepository;
+    private TurnoRepository turnoRepository;
 
     // Crear o actualizar un turno
-    public static Turno saveOrUpdate(Turno turno) {
+    public Turno saveOrUpdate(Turno turno) {
         return turnoRepository.save(turno);
     }
 
@@ -36,18 +36,18 @@ public class TurnoService {
     }
 
     // Actualizar un turno por ID
-public Turno updateTurno(Long id, Turno updateTurno) {
-    return turnoRepository.findById(id).map(turno -> {
-        turno.setFecha(updateTurno.getFecha());
-        turno.setLocal(updateTurno.getLocal());
-        turno.setEstado(updateTurno.getEstado());
-        turno.setCorte(updateTurno.getCorte());
-        turno.setAdicional(updateTurno.getAdicional());
-        turno.setEmailBarbero(updateTurno.getEmailBarbero());
-        turno.setEmailCliente(updateTurno.getEmailCliente());
-        turno.setCliente(updateTurno.getCliente());
-        turno.setHora(updateTurno.getHora());
-        return turnoRepository.save(turno);  // Guardar turno actualizado
-    }).orElseThrow(() -> new RuntimeException("Turno no encontrado"));
-}
+    public Turno updateTurno(Long id, Turno updateTurno) {
+        return turnoRepository.findById(id).map(turno -> {
+            turno.setFecha(updateTurno.getFecha());
+            turno.setLocal(updateTurno.getLocal());
+            turno.setEstado(updateTurno.getEstado());
+            turno.setCorte(updateTurno.getCorte());
+            turno.setAdicional(updateTurno.getAdicional());
+            turno.setEmailBarbero(updateTurno.getEmailBarbero());
+            turno.setEmailCliente(updateTurno.getEmailCliente());
+            turno.setCliente(updateTurno.getCliente());
+            turno.setHora(updateTurno.getHora());
+            return turnoRepository.save(turno);  // Guardar turno actualizado
+        }).orElseThrow(() -> new RuntimeException("Turno no encontrado"));
+    }
 }
